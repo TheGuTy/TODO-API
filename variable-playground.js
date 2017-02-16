40062c593c6902fcb29d3e013c1bede920afdd45
Gabriel Gutierrez <nggutierrez.45@gmail.com>
2017-02-16 15:22:43 -0300

    Add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+//   name: 'guty',
+//   age: 27
+// }
+//
+// function updatePerson (obj) {
+//   // obj = {
+//   //   name: 'guty',
+//   //   age: 28
+//   // };
+//   obj.age = 28;
+// }
+//
+// console.log(person);
+// updatePerson (person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+  gradesArr.push(55);
+  debugger;
+}
+
+addGrades(grades);
+console.log(grades);
