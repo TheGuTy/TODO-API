a64231fd4fc6ada72fde615e97214b9f212fd4fe
Gabriel Gutierrez <nggutierrez.45@gmail.com>
2017-02-17 14:17:04 -0300

    Add sequelize demo

--- server.js
+++ server.js
@@ -114,5 +114,5 @@ app.put('/todos/:id', function(req, res) {
 });
 
 app.listen(PORT, function() {
-  console.log('Express listening on port: ' + PORT + "!");
+console.log('Express listening on port: ' + PORT + "!");
 });
