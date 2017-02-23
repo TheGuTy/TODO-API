60cf8de99c880dae6c783f69e1570080be6a22bd
Gabriel Gutierrez <nggutierrez.45@gmail.com>
2017-02-23 13:29:08 -0300

    Adding todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,53 +19,39 @@ var Todo = sequelize.define('todo', {
   }
 })
 
-sequelize.sync({force: true}).then(function () {
-  console.log('Everything is synced')
+var User = sequelize.define('user', {
+  email: Sequelize.STRING
+});
 
-	Todo.create({
-		description: 'hola mundo'
-	}).then (function (todo) {
-		return Todo.findById(1);
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+	// force: true
+}).then(function() {
+	console.log('Everything is synced');
+
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				completed: true
+			}
+		}).then(function (todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
 	});
-	Todo.create({
-		description: 'ola que aze'
-	});
 
-
-	Todo.findById(2).then(function (todo) {
-		if (todo) {
-			console.log(todo.toJSON());
-		} else {
-			console.log('Todo not found');
-		}
-	});
-
-
-  // Todo.create({
-  //   description: 'Take out trash'
-  // }).then(function (todo) {
-  //   return Todo.create({
-  //     description: 'Clean office'
-  //   })
-  // }).then(function () {
-	// 	// return Todo.findById(1);
-  //   return Todo.findAll({
-  //     where: {
-  //       description: {
-  //         $like: '%trash%'
-  //       }
-  //     }
-  //   })
-  // }).then(function (todos) {
-  //   if (todos) {
-  //     todos.forEach(function (todo) {
-  //       console.log(todo.toJSON())
-  //     })
-  //     console.log(todos)
-  //   } else {
-  //     console.log('No todo found!')
-  //   }
-  // }).catch(function (e) {
-  //   console.log(e)
-  // })
+	// User.create({
+	// 	email: 'andrew@example.com'
+	// }).then(function () {
+	// 	return Todo.create({
+	// 		description: 'Clean yard'
+	// 	});
+	// }).then(function (todo) {
+	// 	User.findById(1).then(function (user) {
+	// 		user.addTodo(todo);
+	// 	});
+	// });
 });
